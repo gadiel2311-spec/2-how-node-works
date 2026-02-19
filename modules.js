3fbc69c2fcc90fe22cc63630e297181836d2e840
gadiel2311-spec <gadiel2311@gmail.com>
2026-02-19 12:17:09 -0500

    Add validation logic

--- modules.js
+++ modules.js
@@ -1,6 +1,12 @@
 // console.log(arguments);
 // console.log(require("module").wrapper);
 
+//module.exports
+
 const C = require("./test-module-1");
 const calc1 = new C();
 console.log(calc1.add(2, 5));
+
+//export
+const calc2 = require("./test-module-2");
+console.log(calc2.multiply(2, 5));
