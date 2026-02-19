ef7b6a9fda6d48d21762a98225004d94468bfc01
gadiel2311-spec <gadiel2311@gmail.com>
2026-02-19 12:23:47 -0500

    Update structure

--- modules.js
+++ modules.js
@@ -8,5 +8,6 @@ const calc1 = new C();
 console.log(calc1.add(2, 5));
 
 //export
-const calc2 = require("./test-module-2");
-console.log(calc2.multiply(2, 5));
+// const calc2 = require("./test-module-2");
+const { add, multiply, divide } = require("./test-module-2");
+console.log(multiply(2, 5));
